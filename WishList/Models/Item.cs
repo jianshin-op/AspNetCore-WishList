e026bfb5597f962f64e119e58267396ff6f8bc68
Jian Shin <jianshin@Jians-MacBook-Pro.local>
2022-09-08 11:42:20 +0200

    step 4

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id;
+
+        [Required(), MaxLength(50)]
+        public string Description;
+
+        public Item()
+        {
+        }
+    }
+}
+
