f445833075d674c3dbe764825a53a62d81e7c3e7
Jian Shin <jianshin@Jians-MacBook-Pro.local>
2022-09-08 13:58:04 +0200

    step 6

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //List<> = _context.Items.ToList;
+            return View("Index", _context.Items);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return Index();
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            Item item = _context.Items.Find(Id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return Index();
+        }
+    }
+}
+
